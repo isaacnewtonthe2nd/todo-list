7a62aea8909928f2d516cab95de95fea99e8b054
isaacnewtonthe2nd <ayotuns73@gmail.com>
2025-01-26 10:11:37 -0800

    Hardcode html and css for the todoList sidebar. Create function to add and remove tasks in tasks.js

--- src/task.js
+++ src/task.js
@@ -1,3 +1,6 @@
+import { tasks } from "./project";
+import { TaskLocalStorage } from "./local-storage";
+
 export class Task {
   constructor (title, description, dueDate, priority) {
     this.title = title;
@@ -28,5 +31,21 @@ export class Task {
   set editPriority (value) {
     this.priority = value;
   }
- }
+}
  
+export function addTask (title, description = '', dueDate = '', priority = '') {
+  const task = new Task (title, description, dueDate, priority);
+  tasks.push(task);
+  TaskLocalStorage(tasks);
+}
+
+
+export function removeTask (index) {
+  for (let i = 0; i < tasks.length; i++) {
+    const currentTask = this.taskArr[index];
+    if (currentTask.title === tasks[i].title) {
+      tasks.splice(i, 1);
+    }
+  }
+  TaskLocalStorage(tasks);
+}
